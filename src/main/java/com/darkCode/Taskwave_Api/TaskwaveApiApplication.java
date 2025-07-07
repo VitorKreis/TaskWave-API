2c08ba9957d086a5037a74753159e8e03928ceaf
VitorKreis <vitor.12richter@gmail.com>
2025-07-07 20:28:53 -0300

    starting the project

---
+++ src/main/java/com/darkCode/Taskwave_Api/TaskwaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.darkCode.Taskwave_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskwaveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskwaveApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darkCode/Taskwave_Api/TaskwaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkCode.Taskwave_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskwaveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
