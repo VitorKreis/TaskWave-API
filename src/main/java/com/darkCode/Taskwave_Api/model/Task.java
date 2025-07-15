d755a4433e870efeff10d8633c11f0903869d165
VitorKreis <vitor.12richter@gmail.com>
2025-07-15 08:44:36 -0300

    func: creating the model for the Task and runnig the docker-compose

---
+++ src/main/java/com/darkCode/Taskwave_Api/model/Task.java
@@ -0,0 +1,54 @@
+package com.darkCode.Taskwave_Api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "done")
+    private Boolean done;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getDone() {
+        return done;
+    }
+
+    public void setDone(Boolean done) {
+        this.done = done;
+    }
+}
